1ea82ff55f1bcfdd6f29a06538ce40a6f6641c33
ivee14 <onuohavictoria07@gmail.com>
2023-06-15 03:57:40 -0700

    c-hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main - Printing
-*
-* Return: Always 0 (Successful)
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual
+  puts("\"Programming is like building a multilingual
 puzzle");
-return (0);
+  return (0);
 }
